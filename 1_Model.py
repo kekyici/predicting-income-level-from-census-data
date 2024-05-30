bf09e3e8856ebded17701de180ae2fd833fecc86
Kek Yi Ci <22102539@siswa.um.edu.my>
2024-05-30 18:29:30 +0000

    kyc

--- 1_Model.py
+++ 1_Model.py
@@ -1,6 +1,8 @@
+
 import streamlit as st
 import numpy as np
 import pandas as pd
+from data_processor import DataProcessor
 from constant import *
 from scipy import stats
 from sklearn.model_selection import train_test_split
@@ -10,17 +12,8 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from data_processor import DataProcessor
 from sklearn.metrics import accuracy_score, classification_report
 
-explore_button = st.sidebar.button("Go to Data Exploration")
-predict_button = st.sidebar.button("Go to Income Prediction")
-#sidebar --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-with st.sidebar:
-    st.success("Select a page above.")
-
-# Load the dataset
-@st.cache_data
 def load_and_process_data():
     processor = DataProcessor(DATA_URL, COLUMN_NAMES, Z_SCORE_THRESHOLD)
     data = processor.data
@@ -29,43 +22,15 @@ def load_and_process_data():
     data = processor.transform_data()
     return data
 
-data = load_and_process_data()                        
-
+data = load_and_process_data()   
 # Title and description
-st.title("Census Income Prediction Web App")
-st.write("Explore the dataset and predict income levels.")
-
-
-# Data Exploration Page
-# Sidebar for data exploration options
-
-st.header("Data Exploration ")
-st.subheader("Summary Statistics")
-st.write(data.describe())
-
-st.subheader("Data Visualization")
-selected_feature = st.selectbox("Select Feature for Visualization", data.columns)
-selected_chart = st.selectbox("Select Chart Type", ["Histogram", "Bar Chart", "Scatter Plot"])
-filtered_data = data[data["income"] == 1] if st.checkbox("Filter High Income") else data
-
-
-
-# Data visualization
-if selected_chart == "Histogram":
-    st.bar_chart(filtered_data[selected_feature].value_counts())
-elif selected_chart == "Bar Chart":
-    st.bar_chart(filtered_data[selected_feature].value_counts())
-else:
-    st.scatter_chart(filtered_data.sample(100), x="age", y="hours_per_week")
-
-
 le = LabelEncoder()
 for column in data.columns:
     if data[column].dtype == 'object':
         data[column] = le.fit_transform(data[column])
 
 
-
+st.title("Census Income Prediction Web App")
 st.header("Income Prediction")
 
 # Sidebar for income prediction
@@ -74,7 +39,7 @@ selected_features = st.multiselect("Select Features for Prediction", data.column
 test_size = st.slider("Test Size", 0.1, 0.5, 0.2)
 
 # Income prediction
-if selected_features and st.button("Train and Predict"):
+if selected_features and st.sidebar.button("Train and Predict"):
     # Data preprocessing
     X = data[selected_features]
     y = data["income"]
